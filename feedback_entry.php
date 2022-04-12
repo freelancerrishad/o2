8bf4bb1d6397a41cfc744cc5d0ba1fbaa739affa
Ferdous Hasan <ferdoushasan119@gmail.com>
2022-04-13 01:54:17 +0600

    Backend done for give feedback

--- feedback_entry.php
+++ feedback_entry.php
@@ -13,4 +13,25 @@
     $email = $_SESSION['email'];
     $name = $_SESSION['name'];
     $feedback= $_POST['feedback'];
+
+    $q1 = "SELECT `name` FROM `user` WHERE email = '$email';";
+
+    $run1 = mysqli_query($con, $q1);
+    $num1 = mysqli_num_rows($run1);
+
+    if ($num1 > 0) {
+        while ($row = $run1->fetch_assoc()) {      
+            $name =  $row['name'] ;
+        }
+    }
+
+    $q2 = "INSERT INTO `feedback`(`name`, `id`, `email`, `feedback`, `upload_time`) VALUES ('$name','','$email','$feedback',Now())";
+            $run = mysqli_query($con, $q2);
+            $_SESSION['add_feedback'] = "Successfully Added in the Database";
+            header("Location:feedback.php");
+
+            exit();
+            
+        
+    }
 ?>
