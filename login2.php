283bedb497b180694d1da3d4cd5e794aeaf54668
abir <achowdhury191255@bscse.uiu.ac.bd>
2022-04-13 01:16:53 +0600

    Back end Done

--- login2.php
+++ login2.php
@@ -4,5 +4,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
     include '_dbconnect.php';
     
     
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $id = $_SESSION['id'];
     
+
+    $sql2 = " SELECT * FROM user WHERE email = '$email' and password = '$password'";
+    $result1 = mysqli_query($con, $sql2);
+    $num1 = mysqli_num_rows($result1);
+
+   
+
+    if ($num1 == 1) {
+        
+        while ($row = $result1->fetch_assoc()) {
+            $role = $row["role"];
+            if($role == 'user'){
+               
+          
+                $_SESSION['admin'] = $email;
+                 header("Location:adminhome.php");
+            
+        
+    }
+    else{
+       
+         header("Location:login.php?id=home");
+          exit(); 
+    }
 }
