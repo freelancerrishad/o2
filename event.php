4b03e3feedcd6be65e143974cdb0499ec04045eb
Jahra <fatemajahra220@gmail.com>
2022-04-14 00:48:57 +0600

    frontend completed of event

---
+++ event.php
@@ -0,0 +1,80 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Add Event</title>
+    <link rel = "icon" href="img/fav.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.4/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="stylesheet" href="css/addproduct.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@900&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+
+
+<section id="cta">
+        <form class="form" action="insert_event.php" method="POST">
+          <div class="row">
+
+            <div class="col-lg-12 col-md-12 col-sm-12" id=col1>
+              <div id="log-title">
+                <h3 class="cta-heading"><i class="fa-solid fa-calendar"></i> Add Event</h3>
+                
+              </div>
+              <div class="form-group">
+                <label for="name">Event Name</label>
+                <input type="text" class="form-control" name="name" placeholder="Event Name" required>
+              </div>
+              <div class="form-group">
+                <label for="Event Image">Event Image</label>
+                <input type="file" class="form-control" name="image" required>
+              </div>
+              <div class="form-group">
+                <label for="Event Location">Event Location</label>
+                <input type="text" class="form-control" name="location" placeholder="Event Location" required>
+              </div>
+              <div class="form-group">
+                <label for="Event Datetime">Event Datetime</label>
+                <input type="datetime-local" class="form-control" name="time" placeholder="Event Datetime" required>
+              </div>
+              
+              <div class="form-group">
+              <label for="Event Details">Event Details</label>
+              <textarea class="form-control" name="description" rows="4" cols="50" placeholder="Enter Event Description" required></textarea>
+              </div>
+
+        
+              <button type="submit" class="btn btn-lg btn-block btn btn-success">Submit</button>
+            </div>
+          </div>
+        </form>
+</section>
+    
+    <footer id="footer">
+    <a id="icon-fb" href="#">
+      <i class="s-icons fa-brands fa-facebook"></i>
+    </a>
+    <a id="icon-insta" href="#">
+      <i class="s-icons fa-brands fa-instagram"></i>
+    </a>
+    <a id="icon-mail" href="mailto:#">
+      <i class="s-icons fa-solid fa-envelope" href=""></i>
+    </a>
+    
+    
+    <p>© Copyright ProjectO2</p>
+    <script src="https://use.fontawesome.com/2c7ebecd35.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    </footer>
+</body>
+</html>
