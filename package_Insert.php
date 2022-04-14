8c77c0fcc30427e24d5ab10b46d2aed48be50d76
Maun Ali <maunali0001@gmail.com>
2022-04-15 01:06:36 +0600

    package insert back end done for entry package

--- package_Insert.php
+++ package_Insert.php
@@ -7,5 +7,17 @@
         $description = $_POST['description'];
         
         
-     
+        $q =  "INSERT INTO `package`(`id`, `name`, `img`, `price`, `description`) VALUES ('','$name','$image','$price','$description');";
+        $run = mysqli_query($con, $q);
+        /*if (mysqli_query($conn, $q)) {
+            echo "Database created successfully with the name newDB";
+        } else {
+            echo "Error creating database: " . mysqli_error($conn);
+        }*/
+          
+        // closing connection
+        $_SESSION['add_package'] = "Successfully Added in the Database";
+        header('location:entry_package_front.php');
+        $con->close();
+    
 ?>  
