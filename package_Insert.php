7b5db9c48c96ace7e0f228731a3b49c15b0174cb
Maun Ali <maunali0001@gmail.com>
2022-04-15 01:04:33 +0600

    dbconnect included for package insert

---
+++ package_Insert.php
@@ -0,0 +1,11 @@
+<?php
+    include '_dbconnect.php';
+    session_start();
+        $name = $_POST['name'];
+        $image = $_POST['image'];
+        $price = $_POST['price'];
+        $description = $_POST['description'];
+        
+        
+     
+?>  
