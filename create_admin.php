817e03bb10e2cdafe6d3ad819b12de9c1ab781d8
abir <achowdhury191255@bscse.uiu.ac.bd>
2022-04-15 20:18:35 +0600

    Alert added successfullys

--- create_admin.php
+++ create_admin.php
@@ -1,4 +1,42 @@
+<?php
+  session_start();
+  $admin = $_SESSION['admin'];
+  if($admin==null){
+    header('location:login.php?id=home');
+  }
+?>
+<?php
+ini_set('error_reporting', 0);
+ini_set('display_errors', 0);
+?>
+<?php
+$check = 0;
 
+
+
+$msg = $_SESSION['success'];
+if($msg == null){
+  $msg = $_SESSION['pasword'];
+  if($msg == null){
+    $msg = $_SESSION['not_success'];
+    $_SESSION['not_success'] = null;
+  }
+  $_SESSION['pasword'] = null;
+}
+$_SESSION['success'] = null;
+
+
+if ($msg == 'Account created successfully') {
+    $check = 1;
+}
+if ($msg == 'Email already used before') {
+  $check = 3;
+}
+if ($msg == 'Password did not matched') {
+  $check = 2;
+}
+
+?>
 <!DOCTYPE html>
 <html>
 
@@ -19,9 +57,53 @@
   <section id="title">
     <div class="container-fluid" id="cf">
         <form class="form" action="signup3.php" method="POST">
+        <?php if ($msg) { ?>
+
+            <?php if ($check == 1) { ?>
+                <div class="row fix">
+                    <div class="col-md-12">
+                        <div class="alert alert-success" role="alert">
+                            <?php echo $msg ?>
+                        </div>
+                    </div>
+
+                </div>
+            <?php } ?>
+            <?php if ($check == 2){ ?>
+                <div class="row fix">
+                    <div class="col-md-12">
+                        <div class="alert alert-danger" role="alert">
+                            <?php echo $msg ?>
+                        </div>
+                    </div>
+
+                </div>
+            <?php } ?>
+
+            <?php if ($check == 3){ ?>
+                <div class="row fix">
+                    <div class="col-md-12">
+                        <div class="alert alert-danger" role="alert">
+                            <?php echo $msg ?>
+                        </div>
+                    </div>
+
+                </div>
+            <?php } ?>
+
+            <?php } ?>  
+        <div class="row">
+        <div class="col-lg-6 col-md-12 col-sm-12" id=col2>
+                <div class="logo">
+                <img class = "logo" src="img/LOGO.png" alt="img">
+                </div>
+                <div class="view">
+                <img class= "pic" src="img/3.svg" alt="img">
+                </div>
+                
         
-
-
+              
+        </div>
             
         
         
@@ -75,8 +157,6 @@
         </form>
     </div>
   </section>
-
-
 
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
