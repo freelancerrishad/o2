febbcbc1c6ab653af3ca726fd250e88dbd14b0fc
Jahra <fatemajahra220@gmail.com>
2022-05-14 01:28:29 +0600

    ava_ajax.php is completed

---
+++ ava_ajax.php
@@ -0,0 +1,24 @@
+<?php
+  $email="user@gmail.com";
+?>
+<?php
+  $link = mysqli_connect("localhost", "root", "", "project_o2");
+  $status = 'OK';
+  $content = [];
+
+if (mysqli_connect_errno()) {
+	$status = 'ERROR';
+	$content = mysqli_connect_error();
+}
+
+$query = "SELECT * FROM user WHERE email='$email'";
+if ($result = mysqli_query($link, $query)) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $content[] = $row;
+    }
+}
+$data = ["status" => $status, "content" => $content];
+
+header('Content-type: application/json');
+echo json_encode($data);
+?>
