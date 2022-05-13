af983ca79b90d57553ecef099c31008a071c10c4
Maun Ali <maunali0001@gmail.com>
2022-05-13 23:57:24 +0600

    request recycle back end done

---
+++ request_recycle_back.php
@@ -0,0 +1,22 @@
+<?php
+   
+    session_start();
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+    include '_dbconnect.php'; 
+    $email = $_SESSION['email'];
+    $address= $_POST['address'];
+    $description= $_POST['description'];
+    
+
+
+
+    $q2 = "INSERT INTO `recycle`(`id`, `email`, `address`, `description`, `status`) VALUES ('', '$email','$address','$description','')";
+            $run = mysqli_query($con, $q2);
+            $_SESSION['add_req'] = "Successfully Added in the Database";
+            header("Location:request_recycle_front.php");
+
+            exit();
+            
+        
+    }
+?>
