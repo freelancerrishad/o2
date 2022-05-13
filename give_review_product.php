96584a62d87d6116d39a2783bca681ba7a7a9060
Jahra <fatemajahra220@gmail.com>
2022-05-14 01:21:57 +0600

    style added in give_review_product.php

---
+++ give_review_product.php
@@ -0,0 +1,51 @@
+<?php
+    $email="user@gmail.com";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Give Review</title>
+    <style>
+
+        body{
+            background-color:aliceblue;
+            background-image :url('R.png');
+            background-position:center;
+            background-size:cover;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+              height: 110vh;
+            
+                }
+                .posts {
+                    display: relative;
+                    background: linear-gradient(
+                        to left top,
+                        rgba(255, 255, 255, 0.9),
+                        rgba(255, 255, 255, 0.9)
+                    );
+                    border-radius: 1rem;
+                    margin: 2rem 3rem;
+                    padding: 1rem;
+                    box-shadow: 6px 6px 20px rgba(122, 122, 122, 0.212);
+                    justify-content: space-between;
+                }
+                .button{
+                    width: 130px;
+                    padding: 10px 0;
+                    text-align: center;
+                    text-shadow: black;
+                    margin: 20px 10px;
+                    border-radius: 25px;
+                    font-weight: bold;
+                    border: 4px solid aqua;
+                    background: #0000009c;
+                    color: white;
+                    cursor: pointer;
+                    position: relative;
+                    overflow: hidden;
+                }
+    </style>
+    </head>
+</html>
