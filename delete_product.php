5fed0b6ae8920d344e51c1769f96a76660f6c198
abir <achowdhury191255@bscse.uiu.ac.bd>
2022-05-14 01:13:52 +0600

    Product delete from database

--- delete_product.php
+++ delete_product.php
@@ -6,4 +6,12 @@
     $queries = array();
     parse_str($_SERVER['QUERY_STRING'], $queries);
     $id = $queries['id'];
+    $sql = "DELETE FROM product WHERE id = $id";
+    
+    $result = mysqli_query($con, $sql);
+    
+    
+    $con->close();
+header("Location: /Project_O2/project_O2/product_list.php");
+exit();
 ?>
