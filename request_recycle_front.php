82fadd3a697c9a624d28d5e765cf3aaa8c6f5613
Maun Ali <maunali0001@gmail.com>
2022-05-13 23:54:11 +0600

    request recycle style added

--- request_recycle_front.php
+++ request_recycle_front.php
@@ -14,7 +14,89 @@ ini_set('display_errors', 0);
     $_SESSION['add_req'] = null;
 ?>
 <!DOCTYPE html>
-
+    <html lang="en">
+        <head> 
+            <link rel = "icon" href="img/fav.png">
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@900&family=Ubuntu&display=swap" rel="stylesheet">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+            <title>Request For Recycling</title>
+            <style>
+                body{
+                background-image:url(img/recycle.jpg);
+                background-position:center;
+                background-size:cover;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+                
+            }
+            *{
+                font-family:  sans-serif;
+                box-sizing: border-box;
+            }
+            form{
+                border: 2px solid #ccc;
+                padding: 92px;
+                background: #19412F99;
+                border-radius: 20px;
+            }
+            h2{
+                font-family: 'Montserrat', sans-serif;
+                text-align: center;
+                margin-bottom: 40px;
+                color: #fff;
+            }
+            input{
+                display: block;
+                border: 2px solid #ccc;
+                width: 95%;
+                padding: 10px;
+                margin: 10px auto;
+                border-radius: 5px;
+            }
+            textarea{
+                display: block;
+                border: 2px solid #ccc;
+                width: 95%;
+                padding: 10px;
+                margin: 10px auto;
+                border-radius: 5px;
+            }
+            label{
+                color: rgb(233, 230, 230);
+                font-size: 18px;
+                padding: 10px;
+            }
+            button {
+                float: right;
+                background: #2e3a38f2;
+                padding: 10px 22px;
+                color: #fff;
+                border-radius: 8px;
+                margin-right: 17px;
+                border: none;
+            }
+            button:hover{
+                opacity: .7;
+                cursor: pointer;
+            }
+            input:hover{
+                opacity: .7;
+            }
+            .dec{
+                color:white;
+            }
+            .c{
+                text-align: center;
+            }
+            </style>
+        </head>
 
       <body>
             <form action="request_recycle_back.php" method="POST">
