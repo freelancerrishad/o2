643746ae1f9785ce4dda87a09168ac06f67e375f
Ferdous Hasan <ferdoushasan119@gmail.com>
2022-05-19 00:43:01 +0600

    Query run for fetching data from my cart

--- viewcart.php
+++ viewcart.php
@@ -7,5 +7,14 @@
 ?>
 <?php
 include '_dbconnect.php';
-
+$q3 =  "SELECT * FROM `cart` WHERE `email`='$email';";
+$run = mysqli_query($con, $q3);
+$html = "";
+if(mysqli_num_rows($run)>0){
+          while($row = $run->fetch_assoc()){
+              $html = $html. "<tr><td>" . $row["name"] . "</td><td>" . $row["details"] . "</td><td>" . '<h4 class="taka">৳</h4>'. $row["price"] . "</td><td>". '<a class="btn btn-lg btn-block btn btn-outline-danger" href="delete_cart.php?id=' . $row["id"] .'"><i class="fa-solid fa-bag-shopping"></i> Delete from Cart</a>' ."</td></tr>";
+             
+          }
+        
+      }
 ?>
