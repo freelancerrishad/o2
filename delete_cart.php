108351687bb0eb7e6aa9ea623cab78b473776918
Ferdous Hasan <ferdoushasan119@gmail.com>
2022-05-19 00:52:36 +0600

    Mysql code written for delete from cart delete completed

--- delete_cart.php
+++ delete_cart.php
@@ -1,3 +1,15 @@
 <?php
     include '_dbconnect.php';
+    
+    session_start();
+    
+    $queries = array();
+    parse_str($_SERVER['QUERY_STRING'], $queries);
+    $id = $queries['id'];
+    $sql = "DELETE FROM cart WHERE id = $id";
+    
+    $result = mysqli_query($con, $sql);
+    
+    
+    $con->close();
 ?>
