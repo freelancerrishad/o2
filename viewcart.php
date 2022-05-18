6e16781089cb0b478c571391d08c6e563cfc7bcb
Ferdous Hasan <ferdoushasan119@gmail.com>
2022-05-19 00:49:03 +0600

    Link added in the button for delete from cart

--- viewcart.php
+++ viewcart.php
@@ -12,7 +12,7 @@ $run = mysqli_query($con, $q3);
 $html = "";
 if(mysqli_num_rows($run)>0){
           while($row = $run->fetch_assoc()){
-              $html = $html. "<tr><td>" . $row["name"] . "</td><td>" . $row["details"] . "</td><td>" . '<h4 class="taka">৳</h4>'. $row["price"] . "</td><td>". '<a class="btn btn-lg btn-block btn btn-outline-danger" href="?id=' . $row["id"] .'"><i class="fa-solid fa-bag-shopping"></i> Delete from Cart</a>' ."</td></tr>";
+              $html = $html. "<tr><td>" . $row["name"] . "</td><td>" . $row["details"] . "</td><td>" . '<h4 class="taka">৳</h4>'. $row["price"] . "</td><td>". '<a class="btn btn-lg btn-block btn btn-outline-danger" href="delete_cart.php?id=' . $row["id"] .'"><i class="fa-solid fa-bag-shopping"></i> Delete from Cart</a>' ."</td></tr>";
              
           }
         
