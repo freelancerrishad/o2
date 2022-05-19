f733b1e9a637cf293515ef3f8b3fb3dfe7d86886
Maun Ali <maunali0001@gmail.com>
2022-05-19 20:04:24 +0600

    Fetching sql added in package_cart

--- package_cart.php
+++ package_cart.php
@@ -4,4 +4,22 @@
   if($email==null){
     header('location:login.php?id=home');
   }
+
+  include '_dbconnect.php';
+  $queries = array();
+    parse_str($_SERVER['QUERY_STRING'], $queries);
+    $id = $queries['id'];
+  $q =  "SELECT `name`,`price`, `description` FROM `package` WHERE id = $id";
+        $run = mysqli_query($con, $q);
+        if(mysqli_num_rows($run)>0){
+            while($row = $run->fetch_assoc()){
+               $name=$row["name"];
+               $price=$row["price"];
+               $details=$row["description"];
+               
+            }
+          
+        }
+
+ 
 ?>
