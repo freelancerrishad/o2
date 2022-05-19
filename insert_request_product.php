18e61afa4ea0e5fc2d81c0d1bba9fa5883f6c7b4
abir <achowdhury191255@bscse.uiu.ac.bd>
2022-05-20 04:12:00 +0600

    Accept requseted product

---
+++ insert_request_product.php
@@ -0,0 +1,29 @@
+<?php
+    include '_dbconnect.php';
+    session_start();
+        $name = $_POST['name'];
+        $image = $_POST['image'];
+        $type = $_POST['type'];
+        $description = $_POST['description'];
+        $price = $_POST['price'];
+        
+        
+        $q =  "INSERT INTO `product`(`id`, `name`, `img`, `price`, `type`, `details`) 
+        VALUES ('','$name','$image','$price','$type','$description')";
+        $run = mysqli_query($con, $q);
+        
+        $_SESSION['add_product'] = "Accept this product and successfully added in database";
+
+       $id = $_SESSION['request_id'];
+        $_SESSION['request_id'] = null;
+       $q1 =  "DELETE FROM `r_product` WHERE `r_product`.`id` = $id";
+       $run = mysqli_query($con, $q1);
+
+        // closing connection
+        header('location:view_product_requests.php');
+        $con->close();
+        
+        
+        
+    
+?>  
